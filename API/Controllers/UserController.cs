c6241e2fa50bbc34d1ec133fe97e281bb2951259
Manan312 <MANAN.1923CO1131@KIET.EDU>
2025-03-22 18:40:58 +0530

    API Creation Start

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UserController(DataContext context)
+        {
+            _context=context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUser()
+        {
+            var users= await _context.Users.ToListAsync();
+            return users;
+        } 
+        [HttpGet("{id:long}")] //api/user/id of user
+        public async Task<ActionResult<AppUser>> GetUsers(Int64 id)
+        {
+            var users=await _context.Users.FindAsync(id);
+            if(users==null)
+            return NotFound();
+            return users;
+        }
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+namespace API.Entities;
+
+public class AppUser
+{
+    public Int64 Id { get; set; }
+    public required string  UserName{get;set;}
+    
+}
